958e6849cc5090da2c358ca42513643f3987f921
Akhil <159180072+akhild24@users.noreply.github.com>
2026-07-24 03:08:06 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+    stack<int> st;
+    stack<int> minSt;
+
+public:
+    MinStack() {}
+
+    void push(int val) {
+        st.push(val);
+        if (minSt.empty() || val <= minSt.top())
+            minSt.push(val);
+        else
+            minSt.push(minSt.top());  // carry forward current min
+    }
+
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+
+    int top() {
+        return st.top();
+    }
+
+    int getMin() {
+        return minSt.top();
+    }
+};
